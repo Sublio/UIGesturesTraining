fada61e3d2fbaed9aaecdc26dbbdebdf2298fed4
Sublio <sublio1260@gmail.com>
2017-06-11 22:53:54 +0600

    Initial Commit

---
+++ GesturesTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GesturesTest
+//
+//  Created by Denis on 11.06.17.
+//  Copyright © 2017 Denis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GesturesTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GesturesTest
+//
+//  Created by Denis on 11.06.17.
+//  Copyright © 2017 Denis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
